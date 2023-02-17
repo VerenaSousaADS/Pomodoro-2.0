3ff5e7ed601d99dc668a361b84cadd434a4b6f70
Verena Sousa <verena.ads19@hotmail.com>
2023-02-17 13:39:08 -0300

    Criando as variaveis

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Controls
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonPlus = document.querySelector('.plus')
+const buttonMinus = document.querySelector('.minus')
+
+// Effects
+const buttonSoundForest = document.querySelector('.forest')
+const buttonSoundRain = document.querySelector('.rain')
+const buttonSoundCoffeeShop = document.querySelector('.coffee-shop')
+const buttonFireplace = document.querySelector('.fireplace')
+
+
